ff15355a9580668b440dceee43ed8c62cb0feedf
charles <cvkimaro1@gmail.com>
2023-02-21 22:48:33 +0300

    emmaus-clean 2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './Components/about/about.component';
-import { ForgetPasswordComponent } from './Components/forget-password/forget-password.component';
 import { HomeComponent } from './Components/home/home.component';
-import { LoginComponent } from './Components/login/login.component';
-import { RegisterComponent } from './Components/register/register.component';
 import { ServicesComponent } from './Components/services/services.component';
 
 const routes: Routes = [
   //Creat routing path and components
-  {path:'login',component:LoginComponent},
   {path:'home',component:HomeComponent},
-  {path:'register',component:RegisterComponent},
-  {path:'login',component:LoginComponent},
-  {path:'forget-password',component:ForgetPasswordComponent},
   {path:'about',component:AboutComponent},
   {path:'services',component:ServicesComponent}
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TextcomponentComponent } from './textcomponent/textcomponent.component';
 import { ImagecomponentComponent } from './imagecomponent/imagecomponent.component';
-import { EmpInfoComponent } from './emp-info/emp-info.component';
-import { FormComponentComponent } from './form-component/form-component.component';
 import { FormsModule } from '@angular/forms';
 import { from } from 'rxjs';
 import { NavbarComponent } from './Components/navbar/navbar.component';
 import { IntroComponent } from './Components/intro/intro.component';
 import { FooterComponent } from './Components/footer/footer.component';
 import { CoursesComponent } from './Components/courses/courses.component';
-import { LoginComponent } from './Components/login/login.component';
 import { HomeComponent } from './Components/home/home.component';
-import { ForgetPasswordComponent } from './Components/forget-password/forget-password.component';
-import { ResetPasswordComponent } from './Components/reset-password/reset-password.component';
-import { RegisterComponent } from './Components/register/register.component';
 import { AboutComponent } from './Components/about/about.component';
 import { ServicesComponent } from './Components/services/services.component';
 import { HttpClientModule } from '@angular/common/http';
@@ -26,17 +20,11 @@ import { HttpClientModule } from '@angular/common/http';
     AppComponent,
     TextcomponentComponent,
     ImagecomponentComponent,
-    EmpInfoComponent,
-    FormComponentComponent,
     NavbarComponent,
     IntroComponent,
     FooterComponent,
     CoursesComponent,
-    LoginComponent,
     HomeComponent,
-    ForgetPasswordComponent,
-    ResetPasswordComponent,
-    RegisterComponent,
     AboutComponent,
     ServicesComponent
     
